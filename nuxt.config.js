bbc872eb255ac4764cd04055228748a5cb0df65c
Luc Varoqui <luc@varoqui.org>
2019-07-09 12:56:58 +0200

    Added auto linting

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,19 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      // Exécuter ESLint lors de la sauvegarde
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
   }
 }
