3a8ddb61b95c701443993bfe5067043b48643665
Luc Varoqui <luc@varoqui.org>
2019-08-24 14:51:43 +0200

    Added Footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,18 @@
 <template>
-  <footer></footer>
+  <footer class="center-text">
+    <div class="element">© Tous droits reservés - La Comédie Musicale 2019</div>
+    <div class="element">
+      Graphismes :
+      <a href="https://www.linkedin.com/in/ramijerbaka/">Rami Jerbaka</a> &amp;
+      Emma Bidault
+    </div>
+    <div class="element">
+      &lt;/&gt; par
+      <a href="https://www.linkedin.com/in/luc-varoqui-897689125/"
+        >Luc Varoqui</a
+      >
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -8,8 +21,21 @@ export default {}
 
 <style lang="scss" scoped>
 footer {
-  background-color: red;
+  background-color: darken($dark-grey-color, 5);
   width: 100%;
-  height: 30px;
+  padding: 1em;
+  scroll-snap-align: end;
+}
+
+.element {
+  padding: 0.2em 0;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  &:hover {
+    text-decoration: underline;
+  }
 }
 </style>
