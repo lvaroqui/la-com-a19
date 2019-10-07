939ef9072d9f29b708ca6464c5c84538c846b184
Luc Varoqui <luc@varoqui.org>
2019-10-07 19:21:39 +0200

    Added pictures, partenaires, fixed Home

--- pages/partenaires.vue
+++ pages/partenaires.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="container">
-    <h1 class="center-text title">Nos partenaires</h1>
-    <div class="flex h-center wrap">
-      <div v-for="sponsor in sponsors" :key="sponsor.name" class="sponsor">
-        <div class="logo" :class="{ noBG: sponsor.noBG }">
-          <img
-            v-if="sponsor.img"
-            :src="sponsor.img"
-            :alt="sponsor.name"
-            :style="`padding: ${sponsor.padding}`"
-          />
-          <img
-            v-else
-            class="picture"
-            src="http://placehold.it/300"
-            :alt="sponsor.name"
-          />
+  <div class="partenaires">
+    <div class="container">
+      <h1 class="center-text title">Nos partenaires</h1>
+      <div class="flex h-center wrap">
+        <div v-for="sponsor in sponsors" :key="sponsor.name" class="sponsor">
+          <div class="logo" :class="{ noBG: sponsor.noBG }">
+            <img
+              v-if="sponsor.img"
+              :src="sponsor.img"
+              :alt="sponsor.name"
+              :style="`padding: ${sponsor.padding}`"
+            />
+            <img v-else class="picture" src="" :alt="sponsor.name" />
+          </div>
         </div>
       </div>
     </div>
@@ -31,6 +28,16 @@ import OiseImg from '~/assets/img/sponsors/oise.png'
 import ADMImg from '~/assets/img/sponsors/antre_de_mondes.png'
 import LPBImg from '~/assets/img/sponsors/les_petits_bilingues.png'
 import CauetteImg from '~/assets/img/sponsors/cauette.png'
+import Compi from '~/assets/img/sponsors/compiegne.jpg'
+import Crous from '~/assets/img/sponsors/crous.png'
+import UTC from '~/assets/img/sponsors/utc.png'
+import SSP from '~/assets/img/sponsors/ssp.png'
+import Decibels from '~/assets/img/sponsors/decibels.png'
+import Picsart from '~/assets/img/sponsors/picsart.jpg'
+import Strava from '~/assets/img/sponsors/strava.png'
+import Polar from '~/assets/img/sponsors/polar.png'
+import Fil from '~/assets/img/sponsors/fil.png'
+import SiMDE from '~/assets/img/sponsors/simde.png'
 
 export default {
   name: 'Partenaires',
@@ -40,12 +47,23 @@ export default {
         { name: 'Les théâtres de Compiègne' },
         { name: 'Le BDE', img: BDEImg },
         { name: 'Le PAE', img: PAEImg, padding: '0.5em' },
+        { name: "L'UTC", img: UTC, padding: '0.5em' },
         { name: 'La Société Générale', img: SoGeImg },
         { name: 'Les Hauts de France', img: HDFImg, padding: '0.5em' },
         { name: "L'Oise", img: OiseImg, padding: '0.5em' },
+        { name: 'La ville de Compiègne', padding: '0.5em', img: Compi },
+        { name: 'Le Crous', img: Crous, padding: '0.5em' },
         { name: "L'antre de mondes", img: ADMImg },
-        { name: 'Les petits bilingues', img: LPBImg, noBG: true },
-        { name: 'La cauette à bière', img: CauetteImg, noBG: true }
+        { name: 'Librairie des Signes' },
+        { name: 'Les petits bilingues', img: LPBImg },
+        { name: 'La cauette à bière', img: CauetteImg },
+        { name: 'SSP', img: SSP },
+        { name: 'Décibels', img: Decibels, padding: '0.5em' },
+        { name: "Pics'Art", img: Picsart },
+        { name: 'Stravaganza', img: Strava, padding: '0.5em' },
+        { name: 'Le Polar', img: Polar },
+        { name: 'Le Fil', img: Fil, padding: '0.5em' },
+        { name: 'SiMDE', img: SiMDE, padding: '0.5em' }
       ]
     }
   }
@@ -53,6 +71,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.partenaires {
+  background-color: $dark-grey-color;
+}
+
 .title {
   font-size: 3em;
   padding: 1em 0;
@@ -64,19 +86,11 @@ export default {
 
 .logo {
   position: relative;
-  height: 300px;
-  width: 300px;
+  height: 250px;
+  width: 250px;
   margin: 0.5em;
   background-color: white;
   box-shadow: 2px 2px 2px black;
-
-  &.noBG {
-    background: none;
-    box-shadow: none;
-    & img {
-      filter: drop-shadow(2px 2px 2px black);
-    }
-  }
 }
 
 img {
